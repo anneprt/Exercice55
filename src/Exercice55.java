d280c7f685cac9308d2fc8d50ebab34e5accd402
Anne <patesdegeek@gmail.com>
2020-04-20 17:57:07 +0200

    instructions composant la fonction

--- src/Exercice55.java
+++ src/Exercice55.java
@@ -1,5 +1,9 @@
 public class Exercice55 {
     public static void main(String[] args) {
-        
+        double pourcentage;
+        int nombreCb;
+        int total;
+        pourcentage = (double) nombreCb / total * 100;
+
     }
 }
